9d912937867e9b663cd6b5dbe136b1b6849a5c15
Tejasw Gupta <guptatejasw2002@gmail.com>
2020-10-31 04:14:56 +0530

    lib/vote: Connected firestore , Added RadioListTile  , Made ListTile work in ListView by using explict int , Added Submit button

--- lib/vote.dart
+++ lib/vote.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class Vote extends StatefulWidget {
@@ -10,6 +11,71 @@ class Vote extends StatefulWidget {
 class _VoteState extends State<Vote> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0.0,
+        backgroundColor: Colors.blueAccent,
+        centerTitle: true,
+        title: Text(
+            "A Vote for a better Nation"), //* Change this to something less cliche
+      ),
+      body: Column(
+        children: [
+          Expanded(child: VoterSelection()),
+          SubmitButton(),
+        ],
+      ),
+    );
+  }
+}
+
+class SubmitButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: FlatButton(
+        color: Colors.blueAccent,
+        child: Text("Submit"),
+        onPressed: () {},
+      ),
+    );
+  }
+}
+
+class VoterSelection extends StatefulWidget {
+  @override
+  _VoterSelectionState createState() => _VoterSelectionState();
+}
+
+class _VoterSelectionState extends State<VoterSelection> {
+  final _collection = FirebaseFirestore.instance.collection('politicianparty');
+  int _selected = 0;
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: _collection.snapshots(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return CircularProgressIndicator();
+        }
+        return ListView.builder(
+          itemBuilder: (context, index) {
+            DocumentSnapshot ds = snapshot.data.docs[index];
+            return RadioListTile<int>(
+              value: index,
+              groupValue: _selected,
+              title: Text(ds.data()['party']),
+              onChanged: (int selection) {
+                setState(() {
+                  _selected = selection;
+                });
+              },
+            );
+          },
+          itemCount: snapshot.data.docs.length,
+        );
+      },
+    );
   }
 }
