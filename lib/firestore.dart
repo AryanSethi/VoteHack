139679f6587ed977542220e9fe5841214179e96e
Tejasw Gupta <guptatejasw2002@gmail.com>
2020-10-31 03:34:40 +0530

    Completed firebase setup ,added plugins, added empty files

---
+++ lib/firestore.dart
@@ -0,0 +1,6 @@
+import 'package:VoteHack/firestore.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class FirestoreService {
+  final _instance = FirebaseFirestore.instance;
+}
---
+++ lib/vote.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Vote extends StatefulWidget {
+  static final id = 'Vote';
+
+  @override
+  _VoteState createState() => _VoteState();
+}
+
+class _VoteState extends State<Vote> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
